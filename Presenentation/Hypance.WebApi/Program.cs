78039b12e993383d3b558982a99cb3702ff8705f
mert atay <merrt.atay@gmail.com>
2022-09-17 21:20:09 +0300

    updated program.cs

--- Presenentation/Hypance.WebApi/Program.cs
+++ Presenentation/Hypance.WebApi/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+ // mert
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
