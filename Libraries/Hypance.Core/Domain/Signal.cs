02ec8f448a91b1ce37fbd76f605dff110f432693
Mahmut Burak Aydın <mahmutburakaydin@gmail.com>
2022-09-17 20:45:26 +0300

    Added Strategy.cs.

---
+++ Libraries/Hypance.Core/Domain/Signal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Signal
+{
+    public string SignalName { get; set; }
+    public List<Indicator> Indıcators { get; set; }
+    public List<Setting> Settings { get; set; }
+    public int Period { get; set; }
+    public enum Interval { get; set; }
+}
