9d781351fc7194ac92fe982990c4937a89e33243
Eren Topacoglu <erentopacoglu21@gmail.com>
2022-09-24 21:35:11 +0300

    Added PreDefinedMarket

---
+++ Libraries/Hypance.Core/Domain/PreDefinedMarket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypance.Core.Domain
+{
+    public class PreDefinedMarket
+    {
+        public string PreDefinedMarket { get; set; }
+    }
+
+}
