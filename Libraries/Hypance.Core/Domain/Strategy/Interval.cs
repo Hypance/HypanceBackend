cd24b1f4de80fd0ab12b2d1443f25544dffd2160
Mahmut Burak Aydın <mahmutburakaydin@gmail.com>
2022-09-25 22:55:46 +0300

    Added Interval.

---
+++ Libraries/Hypance.Core/Domain/Strategy/Interval.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypance.Core.Domain
+{
+    public enum Interval
+    {
+        T_1m = 1,
+        T_5m,
+        T_15m,
+        T_30m,
+        T_1h,
+        T_4h,
+        T_8h,
+        T_1d
+    }
+}
