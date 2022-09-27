03edfdff7c7390e3d1bfbdaac0ff966b4a9e8b87
Eda Şentürk <edasenturkk00@gmail.com>
2022-09-27 16:13:47 +0300

    Added User entities.

---
+++ Libraries/Hypance.Core/Domain/User/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypance.Core.Domain.User
+{
+	public class User
+	{
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int TelegramChanelId { get; set; }
+        public string TelegramApi { get; set; }
+    }
+}
