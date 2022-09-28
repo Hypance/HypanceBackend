bd1ec20af3ab1ca1e2e135770816ed90967bea25
Mustafa Gokturk Caglar <mustafagcaglar@gmail.com>
2022-09-28 21:05:09 +0300

    added BackTest

---
+++ Libraries/Hypance.Core/Domain/Back-Test/BackTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Hypance.Core.Domain.Back_Test
+{
+    public class CreateBackTest
+    {
+        public string BackTestName { get; set; }
+        public List<PredefinedMarket> PredefinedMarket { get; set; }
+        public List<PriceStrategy> PriceStrategy { get; set; }
+        public List<BotType> BotType { get; set; }
+        public List<PredefinedAssets> PredefinedAssets { get; set; }
+        public List<PredefinedStrategy> PredefinedStrategy { get; set; }
+        public List<Formation> Formation { get; set; }
+        public string TrendStrategy { get; set; }
+    }
+}
