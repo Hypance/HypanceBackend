1ab1c34f6873d43325f67ee7d0f113d68d9914e0
Mahmut Burak Aydin <109317317+mahmutburakaydin@users.noreply.github.com>
2022-11-10 22:57:48 +0300

    Added IndicatorController

---
+++ Presenentation/Hypance.WebApi/Controllers/IndicatorController.cs
@@ -0,0 +1,63 @@
+﻿using Hypance.Core.Domain.Strategies;
+using Hypance.Data;
+using Microsoft.AspNetCore.Mvc;
+using Hypance.Services.TelegramApi;
+
+
+
+namespace Hypance.WebApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class IndicatorController : Controller
+    {
+        private readonly IRepository<Indicator> _indicatorRepository;
+
+        public IndicatorController(IRepository<Indicator> indicatorRepository)
+        {
+            _indicatorRepository = indicatorRepository;
+        }
+
+      
+        [HttpGet]
+        public List<Indicator> GetAll()
+        {
+            var model = _indicatorRepository.GetAll();
+            return model.Data;
+        }
+
+    
+        [HttpGet("{id}")]
+        public Indicator Get(int id)
+        {
+            var model = _indicatorRepository.Get(x => x.Id == id);
+            return model.Data;
+        }
+
+     
+        [HttpPost]
+        public IActionResult Post(Indicator indicator)
+        {
+            _indicatorRepository.Add(indicator);
+
+            return Ok();
+        }
+
+      
+        [HttpPut]
+        public IActionResult Put(Indicator indicator)
+        {
+            _indicatorRepository.Update(indicator);
+            return Ok();
+        }
+
+      
+        [HttpDelete]
+        public IActionResult Delete(Indicator indicator)
+        {
+            _indicatorRepository.Delete(indicator);
+            return Ok();
+        }
+    }
+}
