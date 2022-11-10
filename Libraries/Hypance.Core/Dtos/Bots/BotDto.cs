066e58b45252546a71ab77b45d5c6ee3f94ebc1a
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2022-11-10 23:27:38 +0300

    Added BotDto

---
+++ Libraries/Hypance.Core/Dtos/Bots/BotDto.cs
@@ -0,0 +1,20 @@
+﻿using Hypance.Core.Domain.Symbols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypance.Core.Dtos.Bots
+{
+    public class BotDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Interval { get; set; }
+        public bool IsActive { get; set; }
+        public decimal TotalProfit { get; set; }
+        public List<Symbol> Symbols { get; set; }
+        public List<string> StrategyTitles { get; set; }
+    }
+}
