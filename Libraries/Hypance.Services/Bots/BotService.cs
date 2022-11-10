3e2af18cfcaf283d1a8666e1fc442f89f9362b86
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2022-11-10 23:29:09 +0300

    Added BotService

--- Libraries/Hypance.Services/Bots/BotService.cs
+++ Libraries/Hypance.Services/Bots/BotService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Hypance.Core.Domain.Bots;
+using Hypance.Core.Domain.Symbols;
+using Hypance.Core.Dtos.Bots;
+using Hypance.Core.Utilites;
+using Hypance.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +13,45 @@ namespace Hypance.Services.Bots
 {
     public class BotService : IBotService
     {
-        
+        private readonly IRepository<Bot> _botRepository;
+        private readonly IRepository<Symbol> _symbolRepository;
+
+        public BotService(
+            IRepository<Bot> botRepository, IRepository<Symbol> symbolRepository)
+        {
+            _botRepository = botRepository;
+            _symbolRepository = symbolRepository;
+        }
+        public IResult GetAllBots()
+        {
+            IResult result = new Result(false, "");
+            try
+            {
+                List<BotDto> botDtos = new List<BotDto>();
+                var bots = _botRepository.GetAll();
+                if (bots.Success)
+                {
+                    var data = bots.Data.Select(s => 
+                    {
+                        var symbols = _symbolRepository.GetAll(b => s.AssetIds.Contains(b.Id));
+                        return new BotDto()
+                        {
+                            Name = s.Name,
+                            Description = s.Description,
+                            Symbols = symbols.Success ? symbols.Data : new List<Symbol>()
+                        };
+                    }
+                    ).ToList();
+
+                    result = new SuccessDataResult<List<BotDto>>(data);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                result = new ErrorResult(ex.Message);
+            }
+            return result;
+        }
     }
 }
--- Libraries/Hypance.Services/Bots/IBotService.cs
+++ Libraries/Hypance.Services/Bots/IBotService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hypance.Core.Utilites;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace Hypance.Services.Bots
 {
     public interface IBotService
     {
-
+        IResult GetAllBots();
     }
 }
