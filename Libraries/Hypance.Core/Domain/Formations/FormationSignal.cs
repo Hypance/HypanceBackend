eb0494517cc216bfaaf0568cf5e7e8708870272b
Eren Topaçoğlu <erentopacoglu21@gmail.com>
2022-11-12 17:47:23 +0300

    Added FormationSignalController

---
+++ Libraries/Hypance.Core/Domain/Formations/FormationSignal.cs
@@ -0,0 +1,19 @@
+﻿using Hypance.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypance.Core.Domain.Formations
+{
+    public class FormationSignal : BaseEntity
+    {
+        public string IndicatorName { get; set; }
+        public string SymbolName { get; set; }
+        public int Period { get; set; }
+        public Interval Interval { get; set; }
+        public bool Result { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
