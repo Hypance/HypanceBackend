775ff570308a770d1950ca124a4f00390e5387a8
MemoLee <softwarewewo@gmail.com>
2022-11-17 18:51:53 +0300

    jwt identified

--- Presenentation/Hypance.WebApi/Program.cs
+++ Presenentation/Hypance.WebApi/Program.cs
@@ -1,9 +1,12 @@
-﻿using Hypance.Data;
+﻿using System.Text;
+using Hypance.Data;
 using Hypance.ScheduledTask;
 using Hypance.Services;
 using Hypance.Services.Symbols;
-
-
+using Hypance.WebApi.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,13 +23,42 @@ builder.Services.AddSwaggerGen();
 var devCorsPolicy = "devCorsPolicy";
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(devCorsPolicy, builder => {
+    options.AddPolicy(devCorsPolicy, builder =>
+    {
         //builder.WithOrigins("http://localhost:800").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
         builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
         //builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
         //builder.SetIsOriginAllowed(origin => true);
     });
 });
+
+// JWT authentication Aayarlaması
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(o =>
+{
+    o.RequireHttpsMetadata = false;
+    o.SaveToken = true;
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true, // token değerini doğrulanması --> security key
+         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"])),
+        ValidateIssuer = false, // token değerini dağıtıcısı --> www.dagitici.com
+
+        ValidateAudience = false, // token değerlerini kullanıclarını belirtme --> www.kullanici.com
+        ValidateLifetime = true, // time --> 30 munite
+        ValidIssuer = builder.Configuration["Token:Issuer"],
+        ValidAudience = builder.Configuration["Token:Audience"],
+
+        LifetimeValidator = (notBefore, expires, securityToken, validationParameters) => expires != null ? expires > DateTime.UtcNow : false, // time control
+    };
+});
+
+
 var app = builder.Build();
 
 
@@ -42,6 +74,7 @@ app.UseSwaggerUI();
 app.UseCors(devCorsPolicy);
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
